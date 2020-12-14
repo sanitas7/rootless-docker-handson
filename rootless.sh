01de2bc3c09d2ac04b747b5a60ce6a3405340ba1
sanitas <sanitas4257@gmail.com>
2020-12-14 23:04:05 +0900

    [add] rootless dockerをインストールするためのシェルスクリプトを追加した

---
+++ rootless.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+
+# インストーラのダウンロードと実行
+sudo apt-get install uidmap -y
+export FORCE_ROOTLESS_INSTALL=1
+curl -fsSL https://get.docker.com/rootless | sh
+
+# Daemonとクライアントの設定
+systemctl --user start docker
+systemctl --user enable docker
+sudo loginctl enable-linger $(whoami)
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/docker.sock
+
+# rootless contextの作成
+docker context create rootless \
+    --description "for rootless mode" \
+    --docker "host=unix://$XDG_RUNTIME_DIR/docker.sock"
+
+# 再起動
+sudo reboot
