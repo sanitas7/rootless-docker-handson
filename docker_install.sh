2bdbdb7d5f78f444d400923b975cffefa829cfc0
sanitas <39518120+sanitas7@users.noreply.github.com>
2020-12-15 12:07:39 +0900

    [update] Dockerのインストールスクリプトを公式提供のものに変更した

--- docker_install.sh
+++ docker_install.sh
@@ -1,19 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 sudo usermod -aG docker $USER 
